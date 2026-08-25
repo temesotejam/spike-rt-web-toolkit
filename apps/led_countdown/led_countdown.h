ad90b904c1be475dae6b18f2e6dea114482fb4c1
AIAAOOAO <159702851+temesotejam@users.noreply.github.com>
2026-08-25 18:03:59 +0900

    Add explicit app configuration files

---
+++ apps/led_countdown/led_countdown.h
@@ -0,0 +1,10 @@
+#ifndef LED_COUNTDOWN_H
+#define LED_COUNTDOWN_H
+#include <kernel.h>
+#define MAIN_PRIORITY 5
+#define MAIN_STACK_SIZE 4096
+#define STACK_SIZE MAIN_STACK_SIZE
+#ifndef TOPPERS_MACRO_ONLY
+extern void main_task(intptr_t exinf);
+#endif
+#endif
