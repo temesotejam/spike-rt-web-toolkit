bc08c670faef7b5f54e12bcbed0d1329acce2a0d
AIAAOOAO <159702851+temesotejam@users.noreply.github.com>
2026-08-25 18:01:53 +0900

    Add explicit app header and config files

---
+++ apps/motor/motor.h
@@ -0,0 +1,10 @@
+#ifndef MOTOR_H
+#define MOTOR_H
+#include <kernel.h>
+#define MAIN_PRIORITY 5
+#define MAIN_STACK_SIZE 4096
+#define STACK_SIZE MAIN_STACK_SIZE
+#ifndef TOPPERS_MACRO_ONLY
+extern void main_task(intptr_t exinf);
+#endif
+#endif
